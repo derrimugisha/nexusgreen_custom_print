09193b93de4306baf3fb12c610db6cd06b9551ae
derrimugisha <derrimugisha@gmail.com>
2024-04-09 09:38:35 +0300

    adding custom water mark field

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
 # class nexusgreen_custom_print(models.Model):
@@ -16,3 +16,13 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+class NexusGreenWaterMark(models.Model):
+    _inherit = 'purchase.order'
+
+    # Add a field to connect to res.company
+    company_id = fields.Many2one('res.company', string='Company')
+
+    # Related field to access company logo
+    company_logo = fields.Binary(
+        related='company_id.logo', string="Company Logo", readonly=True)
